271970b828a2f7f8a98111906c34e8836273f82f
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2020-04-12 18:46:09 +0530

    Added the UI for generating wallet and implemented its functioning

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
 
 
 class Transaction:
@@ -22,9 +26,23 @@ def make_transaction():
     return render_template('make_transaction.html')
 
 
-@app.route('/view/transaction')
+@app.route('/view/transactions')
 def view_transaction():
-    return render_template('view_transaction.html')
+    return render_template('view_transactions.html')
+
+
+@app.route('/wallet/new')
+def new_wallet():
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(1024,random_gen)
+    public_key = private_key.publickey()
+
+    response = {
+        'private_key': binascii.hexlify(private_key.exportKey(format('DER'))).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.exportKey(format('DER'))).decode('ascii')
+    }
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
