cdcc99e2b4797ea1031da7a51688e8479b90f7cc
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2020-04-12 18:16:53 +0530

    Intial commit

---
+++ blockchain/blockchain.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from time import time
+
+
+class Blockchain:
+    def __init__(self):
+        self.transactions = []
+        self.chain = []
+        # create genesis block
+        self.create_block(0, '00')
+
+    def create_block(self, nonce, previous_hash):
+        block = {'block_number': len(self.chain) + 1,
+                 'timestamp': time(),
+                 'transactions': self.transactions,
+                 'nonce': nonce,
+                 'previous_hash': previous_hash
+                 }
+        # Reset the transactions after adding them
+        self.transactions = []
+        self.chain.append(block)
+
+
+blockchain = Blockchain
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5001, type=int, help="port to listen to")
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port, debug=True)
